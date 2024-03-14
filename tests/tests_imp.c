85a0e6467c20a8723d0c344afa905818df761a4f
CLopMan <cesarlopman@gmail.com>
2024-03-14 22:03:01 +0100

    testing implementation of services

--- tests/tests_imp.c
+++ tests/tests_imp.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include "../src/imp_clave.h"
 
+#define TEST_TUPLE "tuples/0.tuple"
 
 int main (int argc, char **argv) {
     
@@ -37,7 +38,7 @@ int main (int argc, char **argv) {
     case 1:
         //creating newfile
         status = set_value(sample_key, sample_str, sample_N, sample_vector);
-        zero = fopen("tuples/0.tuple", "r");
+        zero = fopen(TEST_TUPLE, "r");
         if (zero == NULL) {
             perror("ERROR:");
             return -1;    
@@ -95,7 +96,7 @@ int main (int argc, char **argv) {
             fprintf(stderr, "Error: return value was %d when 0 was spected\n", status);
             return -1;
         }
-        // non existant key
+        // non existing key
         status = get_value(2, received_str, &received_N, received_vector); 
         if (status != -1 ) {
             fprintf(stderr, "Error: return value was %d when -1 was spected\n", status);
@@ -104,7 +105,7 @@ int main (int argc, char **argv) {
         break;
     case 3:
         status = modify_value(sample_key, Bsample_str, Bsample_N, Bsample_vector);
-        zero = fopen("tuples/0.tuple", "r");
+        zero = fopen(TEST_TUPLE, "r");
         if (zero == NULL) {
             perror("ERROR:");
             return -1;    
@@ -144,12 +145,26 @@ int main (int argc, char **argv) {
         break;
     case 4:
         status = exist(0);
-        if (status != 1) perror("Error: the file exists but it was evaluated as non-existant\n");
+        if (status != 1) perror("Error: the file exists but it was evaluated as non-existing\n");
         status = exist(1);
-        if (status != 0) perror("Error: the file does not exists but it was evaluated as existant\n");
+        if (status != 0) perror("Error: the file does not exists but it was evaluated as existing\n");
         break;
     case 5:
         status = delete_key(sample_key);
+        zero = fopen(TEST_TUPLE, "r");
+        if (zero != NULL) {
+            perror("Error: the file was not erased");
+            fclose(zero);
+
+            return -1;
+        }
+
+        // deleting non-existing file 
+        status = delete_key(1);
+        if(status != -1) {
+            fprintf(stderr, "Error: return value was %d when -1 was spected\n", status);
+            return -1;
+        }
         break;
     default:
         fprintf(stderr, "ERROR: unrecognised op code (%d)", atoi(argv[1]));
